8b68ea23f8307bf712178b9c10076099d5d7dc66
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 18:19:37 +0530

    Added: code of Welcoming

---
+++ src/comparision/LineCompare.java
@@ -0,0 +1,9 @@
+package comparision;
+
+public class LineCompare
+{
+	public static void main(String[] args)
+	{
+		System.out.println("####...Welcome to Line Comparision Computation...####");
+	}
+}
